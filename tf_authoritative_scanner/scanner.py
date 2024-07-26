f1fa179f0e848a9d35538218c7154a09fe33cd5c
Andrew Erickson <aerickson@mozilla.com>
2024-07-26 13:04:17 -0700

    run pre-commit -av

--- tf_authoritative_scanner/scanner.py
+++ tf_authoritative_scanner/scanner.py
@@ -7,11 +7,12 @@ import argparse
 
 VERSION = "1.0.0"  # Define the version constant
 
+
 class TFAuthoritativeScanner:
     authoritative_resources = [
         "google_project_iam_binding",
         "google_folder_iam_binding",
-        "google_organization_iam_binding"
+        "google_organization_iam_binding",
     ]
 
     # less interesting / not verified authoritative resources
@@ -28,7 +29,7 @@ class TFAuthoritativeScanner:
         "google_organization_iam_policy",
         "google_container_cluster",
         "google_pubsub_topic",
-        "google_cloud_run_service"
+        "google_cloud_run_service",
     ]
 
     exception_comment_pattern = re.compile(r"#\s*terraform_authoritative_scanner_ok")
@@ -39,7 +40,7 @@ class TFAuthoritativeScanner:
         self.verbose = verbose
 
     def check_file_for_authoritative_resources(self, file_path):
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             lines = file.readlines()
 
         authoritative_lines = []
@@ -52,7 +53,9 @@ class TFAuthoritativeScanner:
                 continue
             # Check if the line contains any authoritative resource and is not excepted
             if any(resource in line for resource in self.authoritative_resources):
-                if not self.exception_comment_pattern.search(line) and not self.exception_comment_pattern.search(previous_line):
+                if not self.exception_comment_pattern.search(
+                    line
+                ) and not self.exception_comment_pattern.search(previous_line):
                     authoritative_lines.append((line_number, stripped_line))
             previous_line = stripped_line
 
@@ -64,37 +67,63 @@ class TFAuthoritativeScanner:
         for root, dirs, files in os.walk(self.directory):
             if not self.include_dotdirs:
                 # Exclude directories starting with '.'
-                dirs[:] = [d for d in dirs if not d.startswith('.')]
+                dirs[:] = [d for d in dirs if not d.startswith(".")]
             for file in files:
                 if file.endswith(".tf"):
                     total_files += 1
                     file_path = os.path.join(root, file)
-                    authoritative_lines = self.check_file_for_authoritative_resources(file_path)
+                    authoritative_lines = self.check_file_for_authoritative_resources(
+                        file_path
+                    )
                     if authoritative_lines:
                         all_authoritative_lines.append((file_path, authoritative_lines))
         return all_authoritative_lines, total_files
 
     def run(self):
-        all_authoritative_lines, total_files = self.check_directory_for_authoritative_resources()
+        all_authoritative_lines, total_files = (
+            self.check_directory_for_authoritative_resources()
+        )
         if all_authoritative_lines:
             if self.verbose:
                 for file_path, lines in all_authoritative_lines:
                     for line_number, line in lines:
                         print(f"AUTHORITATIVE: {file_path}:{line_number}: {line}")
             authoritative_files = len(all_authoritative_lines)
-            print(f"ERROR: {authoritative_files} of {total_files} scanned files are authoritative.")
+            print(
+                f"ERROR: {authoritative_files} of {total_files} scanned files are authoritative."
+            )
             sys.exit(1)
         else:
             authoritative_files = len(all_authoritative_lines)
-            print(f"PASS: {authoritative_files} of {total_files} scanned files are authoritative.")
+            print(
+                f"PASS: {authoritative_files} of {total_files} scanned files are authoritative."
+            )
             sys.exit(0)
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Static analysis of Terraform files for authoritative GCP resources.")
+    parser = argparse.ArgumentParser(
+        description="Static analysis of Terraform files for authoritative GCP resources."
+    )
     parser.add_argument("directory", help="Directory path containing Terraform files")
-    parser.add_argument("-i", "--include-dotdirs", action="store_true", help="Include directories starting with a dot")
-    parser.add_argument("--version", action="version", version=f"%(prog)s {VERSION}", help="Show program's version number and exit")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Show detailed output of authoritative resource lines")
+    parser.add_argument(
+        "-i",
+        "--include-dotdirs",
+        action="store_true",
+        help="Include directories starting with a dot",
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {VERSION}",
+        help="Show program's version number and exit",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Show detailed output of authoritative resource lines",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.directory):
@@ -103,6 +132,7 @@ def main():
 
     scanner = TFAuthoritativeScanner(args.directory, args.include_dotdirs, args.verbose)
     scanner.run()
+
 
 if __name__ == "__main__":
     main()
--- tf_authoritative_scanner/scanner_test.py
+++ tf_authoritative_scanner/scanner_test.py
@@ -4,10 +4,11 @@ import tempfile
 import subprocess
 from tf_authoritative_scanner.scanner import TFAuthoritativeScanner
 
+
 @pytest.fixture
 def temp_tf_file():
     with tempfile.NamedTemporaryFile(suffix=".tf", delete=False) as temp_file:
-        temp_file.write(b'''
+        temp_file.write(b"""
         resource "google_project_iam_binding" "binding" {
             project = "my-project"
             role    = "roles/viewer"
@@ -15,17 +16,18 @@ def temp_tf_file():
                 "user:viewer@example.com",
             ]
         }
-        ''')
+        """)
         temp_file.close()
         yield temp_file.name
     os.remove(temp_file.name)
+
 
 @pytest.fixture
 def temp_tf_dir(temp_tf_file):
     temp_dir = tempfile.TemporaryDirectory()
     tf_file_path = os.path.join(temp_dir.name, os.path.basename(temp_tf_file))
-    with open(tf_file_path, 'w') as f:
-        f.write('''
+    with open(tf_file_path, "w") as f:
+        f.write("""
         resource "google_project_iam_binding" "binding" {
             project = "my-project"
             role    = "roles/viewer"
@@ -33,14 +35,15 @@ def temp_tf_dir(temp_tf_file):
                 "user:viewer@example.com",
             ]
         }
-        ''')
+        """)
     yield temp_dir.name
     temp_dir.cleanup()
+
 
 @pytest.fixture
 def temp_tf_file_with_exception_same_line():
     with tempfile.NamedTemporaryFile(suffix=".tf", delete=False) as temp_file:
-        temp_file.write(b'''
+        temp_file.write(b"""
         resource "google_project_iam_binding" "binding" { # terraform_authoritative_scanner_ok
             project = "my-project"
             role    = "roles/viewer"
@@ -48,15 +51,16 @@ def temp_tf_file_with_exception_same_line():
                 "user:viewer@example.com",
             ]
         }
-        ''')
+        """)
         temp_file.close()
         yield temp_file.name
     os.remove(temp_file.name)
 
+
 @pytest.fixture
 def temp_tf_file_with_exception_previous_line():
     with tempfile.NamedTemporaryFile(suffix=".tf", delete=False) as temp_file:
-        temp_file.write(b'''
+        temp_file.write(b"""
         # terraform_authoritative_scanner_ok
         resource "google_project_iam_binding" "binding" {
             project = "my-project"
@@ -65,26 +69,34 @@ def temp_tf_file_with_exception_previous_line():
                 "user:viewer@example.com",
             ]
         }
-        ''')
+        """)
         temp_file.close()
         yield temp_file.name
     os.remove(temp_file.name)
+
 
 def test_check_file_for_authoritative_resources(temp_tf_file):
     scanner = TFAuthoritativeScanner(temp_tf_file, include_dotdirs=False)
     authoritative_lines = scanner.check_file_for_authoritative_resources(temp_tf_file)
     assert len(authoritative_lines) > 0
 
+
 def test_check_directory_for_authoritative_resources(temp_tf_dir):
     scanner = TFAuthoritativeScanner(temp_tf_dir, include_dotdirs=False)
-    all_authoritative_lines, total_files = scanner.check_directory_for_authoritative_resources()
+    all_authoritative_lines, total_files = (
+        scanner.check_directory_for_authoritative_resources()
+    )
     assert total_files == 1
     assert len(all_authoritative_lines) > 0
 
+
 def test_directory_not_exists():
     with pytest.raises(SystemExit):
-        scanner = TFAuthoritativeScanner("non_existent_directory", include_dotdirs=False)
+        scanner = TFAuthoritativeScanner(
+            "non_existent_directory", include_dotdirs=False
+        )
         scanner.run()
+
 
 def test_run_verbose(temp_tf_dir, capsys):
     scanner = TFAuthoritativeScanner(temp_tf_dir, include_dotdirs=False, verbose=True)
@@ -93,6 +105,7 @@ def test_run_verbose(temp_tf_dir, capsys):
     captured = capsys.readouterr()
     assert "AUTHORITATIVE" in captured.out
 
+
 def test_run_non_verbose(temp_tf_dir, capsys):
     scanner = TFAuthoritativeScanner(temp_tf_dir, include_dotdirs=False, verbose=False)
     with pytest.raises(SystemExit):
@@ -100,30 +113,34 @@ def test_run_non_verbose(temp_tf_dir, capsys):
     captured = capsys.readouterr()
     assert "1 of 1 scanned files are authoritative" in captured.out
 
+
 def test_exception_comment_same_line(temp_tf_file_with_exception_same_line):
-    scanner = TFAuthoritativeScanner(temp_tf_file_with_exception_same_line, include_dotdirs=False)
-    authoritative_lines = scanner.check_file_for_authoritative_resources(temp_tf_file_with_exception_same_line)
+    scanner = TFAuthoritativeScanner(
+        temp_tf_file_with_exception_same_line, include_dotdirs=False
+    )
+    authoritative_lines = scanner.check_file_for_authoritative_resources(
+        temp_tf_file_with_exception_same_line
+    )
     assert len(authoritative_lines) == 0
+
 
 def test_exception_comment_previous_line(temp_tf_file_with_exception_previous_line):
-    scanner = TFAuthoritativeScanner(temp_tf_file_with_exception_previous_line, include_dotdirs=False)
-    authoritative_lines = scanner.check_file_for_authoritative_resources(temp_tf_file_with_exception_previous_line)
+    scanner = TFAuthoritativeScanner(
+        temp_tf_file_with_exception_previous_line, include_dotdirs=False
+    )
+    authoritative_lines = scanner.check_file_for_authoritative_resources(
+        temp_tf_file_with_exception_previous_line
+    )
     assert len(authoritative_lines) == 0
 
+
 def test_main_function(temp_tf_dir):
-    result = subprocess.run(
-        ["tfas", temp_tf_dir],
-        capture_output=True,
-        text=True
-    )
+    result = subprocess.run(["tfas", temp_tf_dir], capture_output=True, text=True)
     assert "1 of 1 scanned files are authoritative" in result.stdout
     assert result.returncode == 1
 
+
 def test_main_function_invalid():
-    result = subprocess.run(
-        ["tfas", 'bad_path_xyy888'],
-        capture_output=True,
-        text=True
-    )
+    result = subprocess.run(["tfas", "bad_path_xyy888"], capture_output=True, text=True)
     # assert "1 of 1 scanned files are authoritative" in result.stdout
     assert result.returncode == 1
